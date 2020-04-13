14c12e89bcec94ae36437310f4d005c6f4efff34
Held0n <764165887@qq.com>
2020-04-14 02:15:19 +0800

    Chapter 6 done

---
+++ chapter-06/recipe-05/example.cpp
@@ -0,0 +1,8 @@
+#include "version.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "This is output from code v" << PROGRAM_VERSION
+    << std::endl;
+    std::cout << "Hello CMake world!" << std::endl;
+}
